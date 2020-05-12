09d67100daadf2316c6cc18bf11a71b22883e8d4
Victor Williams <victor.williams1030@gmail.com>
2020-05-12 21:27:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
-  sentence_reversed = []
+  reversed = []
   sentence_array.collect do |phrase|
     sentence_reversed.push(phrase.reverse)
   end 
